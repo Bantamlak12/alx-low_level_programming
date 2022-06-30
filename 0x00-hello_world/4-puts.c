5f05020871656e27d63cadd7aa9cd85bcdb47813
Bantamlak12 <neoluka315@gmail.com>
2022-06-30 10:09:18 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-/**
+/***
  * main: print a string puts function
  *
  * Return 0 on success
  *
- */
+ ***/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
