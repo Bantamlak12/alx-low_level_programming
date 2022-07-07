289a8f7037bba8a500ecc39080e1bc07369cf4d1
Bantamlak12 <neoluka315@gmail.com>
2022-07-07 10:20:01 -0700

    Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(\n");/* 'i' is not incrementing*/
+	printf("Infinite loop incoming :(\n"); /* 'i' is not incrementing*/
 
 	i = 0;
 
 	while (i < 10)
 	{
 		putchar(i);
+		i++;
 	}
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
