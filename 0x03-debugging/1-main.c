048a38d9875615e62fc5a88c9da38f02da95b0a5
Bantamlak12 <neoluka315@gmail.com>
2022-07-07 09:04:11 -0700

    Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 
         printf("Infinite loop incoming :(\n");
 
-        i = 0; /* 'i' was not incrementing, that was the bug */
+        i = 0: 
 
         while (i < 10);
         {
