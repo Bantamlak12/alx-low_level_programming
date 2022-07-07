cc2515643cdae92cae101d9df0a6466dc15ec5d4
Bantamlak12 <neoluka315@gmail.com>
2022-07-07 03:03:52 -0700

    Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 
         i = 0;
 
-        while (i < 10)/*i is not incrementing, it always return to 0*/
+        while (i < 10)
 
         {
 
@@ -20,5 +20,5 @@ int main(void)
         }
         printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+        return (0); /*i is not incrementing, it always return o*/
 }
