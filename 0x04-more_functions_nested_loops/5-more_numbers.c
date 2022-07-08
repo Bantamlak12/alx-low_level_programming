d99e1429aca19a1c10e89df8abb836b790473900
Bantamlak12 <neoluka315@gmail.com>
2022-07-08 15:18:37 -0700

    Numbering

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - a function that prints 10 times the numbers, from 0 to 14
+ */
+void more_numbers(void)
+{
+	int hor, ver;
+
+	for (ver = 0; ver <= 9; ver++)
+	{
+		for (hor = 0; hor <= 14; hor++)
+		{
+
+			if (hor > 9)
+
+			_putchar((hor / 10) +'0');
+
+		_putchar((hor % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
