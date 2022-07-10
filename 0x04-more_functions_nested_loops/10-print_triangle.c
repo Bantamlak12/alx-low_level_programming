2961691aa0b0665d3578ef32a0c94e90b97d757d
Bantamlak12 <neoluka315@gmail.com>
2022-07-10 12:37:47 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,14 +9,14 @@ void print_triangle(int size)
 
 	for (i = 1; i <= size; i++)
 	{
-	for (space = 1; space <= (size - i); space++)
-	{
-		_putchar(' ');
-	}
-	for (j = 1; j <= i; j++)
-	{
-		_putchar('#');
-	}
-	_putchar('\n');
+		for (space = 1; space <= (size - i); space++)
+		{
+			_putchar(' ');
+		}
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
 	}
 }
