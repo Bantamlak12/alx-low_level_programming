bcfe439800345b263182a7692c38cf0b3eb684d8
Bantamlak12 <neoluka315@gmail.com>
2022-07-10 13:48:00 -0700

    integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,17 +9,8 @@ void print_number(int n)
 	{
 		_putchar('-');
 		n = -n;
-		_putchar('\n');
 	}
-	else if (n == 0)
-	{
-		_putchar(0);
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar('+');
-		n = n;
-		_putchar('\n');
-	}
+	if ((n / 10) > 0)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
 }
