c6f02495b1d368558e48a54c6ce2c137ac52209f
Bantamlak12 <neoluka315@gmail.com>
2022-07-15 03:49:12 -0700

    c is fun

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array to be reversed 
+ * @n: number of integer contents
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp, i;
+
+	for (i = 0; i <= n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tmp;
+	}
+}
