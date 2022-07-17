0633521a7281be6e70dd24f4e98677030bec8da6
Bantamlak12 <neoluka315@gmail.com>
2022-07-17 00:15:50 -0700

    c is fun

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,18 +4,35 @@
 /**
  * cap_string - capitalizes all words of a string
  * @str: a strinh to be capitalized
+ * Return: the string
  */
 
 char *cap_string(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i])
 	{
-		if (str[i] >= 'a' &&  str[i] <= 'z')
-		{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+
+		if (str[i - 1] == ' '||
+			str[i - 1] == '\t' ||
+			str[i - 1] == '\n' ||
+			str[i - 1] == ',' ||
+			str[i - 1] == ';' ||
+			str[i - 1] == '.' ||
+			str[i - 1] == '!' ||
+			str[i - 1] == '?' ||
+			str[i - 1] == '"' ||
+			str[i - 1] == '(' ||
+			str[i - 1] == ')' ||
+			str[i - 1] == '{' ||
+			str[i - 1] == '}' ||
+			str[i] == 0)
+
 			str[i] = str[i] - 32;
-		}
+		i++;
 	}
 	return (str);
 }
