987e12525efca8207a3c206c0cb7a38fc94afa5a
Bantamlak12 <neoluka315@gmail.com>
2022-07-17 09:20:21 -0700

    c is fun

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
+ * cap_string - capitalize all words of the string
  * @str: a string to be capitalized
  * Return: the string
  */
@@ -12,27 +12,30 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
-		{
-			if (str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ',' ||
-				str[i - 1] == ';' ||
-				str[i - 1] == '.' ||
-				str[i - 1] == '!' ||
-				str[i - 1] == '?' ||
-				str[i - 1] == '"' ||
-				str[i - 1] == '(' ||
-				str[i - 1] == ')' ||
-				str[i - 1] == '{' ||
-				str[i - 1] == '}' ||
-				str[i] == 0)
 
-				str[i] = str[i] - 32;
-		}
-		i++;
+	if (str[0] >= 'a' && str[0] <= 'z')
+	{
+		str[0] = str[0] - 32;
+	}
+	if (str[i] == ' ' ||
+		str[i] == '\t' ||
+		str[i] == '\n' ||
+		str[i] == ',' ||
+		str[i] == ';' ||
+		str[i] == '.' ||
+		str[i] == '!' ||
+		str[i] == '?' ||
+		str[i] == '"' ||
+		str[i] == '(' ||
+		str[i] == ')' ||
+		str[i] == '{' ||
+		str[i] == '}')
+
+	if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+	{
+		str[i + 1] = str[i + 1] - 32;
+	}
+	i++;
 	}
 	return (str);
 }
