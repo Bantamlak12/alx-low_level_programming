488beae201d87fd4199276ae8c51117e8025b5e7
Bantamlak12 <neoluka315@gmail.com>
2022-07-18 19:15:52 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr -
+ * @s: a string to be scanned
+ * @c: a character to be searched in s
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i >= '\0'; i++)
+	{
+		if (s[i] == c)
+		s = s +  i;
+		return (s);
+	}
+
+	return ('\0');
+}
