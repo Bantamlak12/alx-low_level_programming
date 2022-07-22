7a9e3e6ea18f8761b318f2e5017c4d39603a51f3
Bantamlak12 <neoluka315@gmail.com>
2022-07-22 12:44:42 -0700

    done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 /**
- *
+ * main - pass number of argument
+ * @argc: arguments passed through command line
+ * @argv: unused array of character pointer
+ * @n: a variable to pass integer value
+ * Return: Always (0)
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
