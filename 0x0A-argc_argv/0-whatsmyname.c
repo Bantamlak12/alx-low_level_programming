de5c01e94f723486b04165dfc358b21aed0d748a
Bantamlak12 <neoluka315@gmail.com>
2022-07-22 12:05:58 -0700

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 /**
- *
- *
+ * main - prints name of the program
+ * @argc: count number of argument passed to the program
+ * @argv: a pointer array
+ * Return: 0 for int type function
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
