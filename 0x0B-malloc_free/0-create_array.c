d9a5e23057e30ebb61bf821121fbdcca34b52f21
Bantamlak12 <neoluka315@gmail.com>
2022-07-25 14:37:42 -0700

    commited

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,14 +15,14 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	if (size == 0)
-	{
-		return (NULL);
-	}
 	ptr = malloc(sizeof(char) * size);
 	for (i = 0; i < size; ++i)
 	{
 		ptr[i] = c;
+	}
+	if (ptr == NULL)
+	{
+		return (NULL);
 	}
 return (ptr);
 }
