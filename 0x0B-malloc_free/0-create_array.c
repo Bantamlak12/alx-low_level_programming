b50d377acea2a392a7d0823e15e8af75b1bdb86b
Bantamlak12 <neoluka315@gmail.com>
2022-07-25 14:21:04 -0700

    commited

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include<stdlib.h>
+/**
+ * create_array - array of character
+ * @size: the size of the array to be initialized with
+ * @c: The character to initialize the array 
+ * Return: NULL if size is equal to zero or if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(char) * size);
+	for (i = 0; i < size; ++i)
+	{
+		ptr[i] = c;
+	}
+return (ptr);
+}
