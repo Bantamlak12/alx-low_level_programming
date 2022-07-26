f3da1b764b7ecd22ca2e03be44c44dcec82e01e8
Bantamlak12 <neoluka315@gmail.com>
2022-07-26 16:12:18 -0700

    commited

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,6 @@ char *str_concat(char *s1, char *s2)
 	int index, index2 = 0, size = 0;
 	char *ptr;
 
-	if (s1 == NULL && s2 == NULL)
-		return ("");
-
 	for (index = 0; s1[index] || s2[index]; index++)
 		size++;
 	ptr = malloc(sizeof(char) * size);
