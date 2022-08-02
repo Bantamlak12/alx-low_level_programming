ed0aed8fbe60a6a62c3e5f4959bcbf17bf427d2d
Bantamlak12 <neoluka315@gmail.com>
2022-08-02 14:37:06 -0700

    commited

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include<stdlib.h>
+/**
+ * free_dog - frees a dog
+ * @d: a pointer to dog
+ * Return: Nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
