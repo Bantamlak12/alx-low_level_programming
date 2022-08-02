2dbe02df8b56d7a2fb2e2445ec0315877b465c53
Bantamlak12 <neoluka315@gmail.com>
2022-08-02 09:19:21 -0700

    commited

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+	typedef struct dog my_dog;
+
+	my_dog *d(char *name, float age, char *owner);
+
+#endif
