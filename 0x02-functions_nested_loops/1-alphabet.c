cc66e705a5aed11226a4b47e1dded9754a218493
Bantamlak <neoluka315@gmail.com>
2022-09-13 16:13:55 +0000

    Functions, nested loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase,
+ *
+ * Return: Nothing 
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 97; ch <= 122; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
