e23f6efdd3ba10ed6181eab17e383562e11efe6f
Bantamlak <neoluka315@gmail.com>
2022-09-15 12:31:05 -0700

    fizz-buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,20 +10,19 @@ int main(void)
 
 	for (n = 0; n <= 100; n++)
 	{
-		 if ((n % 3 == 0) && (n % 5 == 0))
-			 printf("FizzBuzz");
-		 else if (n % 3 == 0)
-			 printf("Fizz");
-		 else if (n % 5 == 0)
-			 printf("Buzz");
-		 else
-			  printf("%d", n);
-		 if (n == 100)
-			 continue;
-		 printf(" ");
-
+		if ((n % 3 == 0) && (n % 5 == 0))
+			printf("FizzBuzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", n);
+		if (n == 100)
+			continue;
+		printf(" ");
 	}
 	printf("\n");
-	
+
 	return (0);
 }
