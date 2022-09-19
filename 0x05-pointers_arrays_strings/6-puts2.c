59511da6c2093d3ff4e1070db4294d54b7a6ad15
Bantamlak <neoluka315@gmail.com>
2022-09-19 06:43:50 -0700

    betty style corr.

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include<string.h>
 /**
- * puts2 - prints every other character of a string, starting with the first character
+ * puts2 - prints character of a string
  *
  * @str: string
  */
