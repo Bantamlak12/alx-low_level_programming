58bcc65b13e344c841c5c07a2f990fbeb4dc4172
Bantamlak <neoluka315@gmail.com>
2022-09-23 07:41:44 -0700

    betty style and doc. corr.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,11 +2,12 @@
 /**
  * cap_string - a function that capitalizes all words of a string
  * @str: a string to be capitalized
+ * Return: the result
  */
 char *cap_string(char *str)
 {
 	int index = 0;
-	
+
 	while (str[index] != '\0')
 	{
 		if (str[0] >= 'a' && str[0] <= 'z')
@@ -26,7 +27,7 @@ char *cap_string(char *str)
 				str[index] == ')' ||
 				str[index] == '{' ||
 				str[index] == '}')
-		if (str[index + 1] >= 'a' && str[index+1] <= 'z')
+		if (str[index + 1] >= 'a' && str[index + 1] <= 'z')
 		{
 			str[index + 1] = str[index + 1] - 32;
 		}
