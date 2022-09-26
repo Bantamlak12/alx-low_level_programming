a559d2abd90197e3bb199d3273b48f3922093d58
Bantamlak <neoluka315@gmail.com>
2022-09-26 07:31:24 -0700

    line of code corrected

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,10 +9,9 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i, len;
+	unsigned int i;
 
-	len = strlen(src) + n;
-	for (i = 0; i < len; i++)
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
 	return (dest);
 }
