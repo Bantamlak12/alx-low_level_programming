b7abd4315a69ab9a3cdc73ae0e0f4d0f3698640e
Bantamlak <neoluka315@gmail.com>
2022-09-27 15:20:38 -0700

    locate a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,13 +10,16 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int i = 0;
+
 	if (*needle == 0)
 		return (haystack);
 
 	while (*haystack)
 	{
-		if (*haystack == *needle)
+		if (haystack[i] == needle[i])
 		{
+			if (needle[i + 1] != '\0')
 				return (haystack);
 		}
 		haystack++;
