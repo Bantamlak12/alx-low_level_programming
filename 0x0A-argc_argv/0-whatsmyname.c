809af2617b62286aa07d23db0bbe60d5ccfc0e97
Bantamlak <neoluka315@gmail.com>
2022-09-30 09:39:04 +0000

    betty documentation

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
+ * main - a program that prints its name
+ * @argv: a pointer array that points to argument passed
+ * @argc: number of arguments passed
+ * Return: Always 0.
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
