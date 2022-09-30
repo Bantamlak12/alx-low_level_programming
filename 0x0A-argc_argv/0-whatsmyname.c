b517e70934f7e43cf021decbd5a19d2277b9b735
Bantamlak <neoluka315@gmail.com>
2022-09-30 09:32:57 +0000

    using array of argv at index of 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
