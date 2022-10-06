d082636adf0e6699c67e08e1de8fcd65eb7e079e
Bantamlak <neoluka315@gmail.com>
2022-10-06 14:16:45 -0700

    loop removed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,11 +21,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		return ("");
 
-	/* len = strlen(s1) + n + 1; */
+	len = strlen(s1) + n + 1;
 
-	for (i = 0; s1[i]; i++)
-		len++;
-	nconcat = malloc(sizeof(char) * (len + 1));
+	nconcat = malloc(sizeof(char) * len);
 
 	if (nconcat == NULL)
 		return (NULL);
