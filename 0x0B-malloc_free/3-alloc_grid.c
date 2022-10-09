caa654eaca269f0ef7f366942934ecb21dc449e0
Bantamlak <neoluka315@gmail.com>
2022-10-09 20:20:59 +0000

    betty style

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,8 +12,8 @@ int **alloc_grid(int width, int height)
 {
 	int i, j;
 	int **grid;
-	
-	if (width <= 0 || height <- 0)
+
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	grid = malloc(sizeof(int) * height);
