06c985472b5727300b44391c5d1d438a3b9f6dad
Bantamlak <neoluka315@gmail.com>
2022-10-12 05:45:11 +0000

    when age is less than zero

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,10 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n)");
-		printf("Age: %.6f\n", d->age);
+		if (d->age < 0)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %.6f\n", d->age);
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
