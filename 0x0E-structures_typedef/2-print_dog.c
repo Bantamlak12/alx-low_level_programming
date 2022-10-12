0873ce7f5db36333de9e7a9dfcfb56d07c4e646c
Bantamlak <neoluka315@gmail.com>
2022-10-12 05:20:17 +0000

    considering d when it's different from NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,17 +11,15 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d != NULL)
-		{
-			if (d->name != NULL)
-				printf("Name: %s\n", d->name);
-			else
-				printf("(Name: (nil)\n)");
-
-			printf("Age: %f\n", d->age);
-
-			if (d->owner != NULL)
-				printf("Owner: %s\n", d->owner);
-			else
-				printf("Owner: (nil)\n)");
-		}
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("(Name: (nil)\n)");
+		printf("Age: %f\n", d->age);
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n)");
+	}
 }
