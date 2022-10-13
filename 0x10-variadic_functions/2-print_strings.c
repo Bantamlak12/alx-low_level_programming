53e07bd579f2d18570e9679f1ef336fe32b7f0f0
Bantamlak <neoluka315@gmail.com>
2022-10-13 18:57:03 +0000

    line checked for betty style and doc

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 
 /**
- * print_string - a function that prints strings
+ * print_strings - a function that prints strings
  * @separator: is the string to be printed between the strings
  * @n: is the number of strings passed to the function
  * Return: Nothing.
@@ -12,18 +12,20 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+	char *str;
 
 	va_list ap;
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(ap, char *));
-		if (i != (n - 1) && separator != NULL)
+		str = va_arg(ap, char *);
+			printf("%s", str);
+		if ((i != (n - 1)) && (separator != NULL))
 			printf("%s", separator);
-		if (n == '\0')
+		if (str == NULL)
 			printf("(nil)");
 	}
-	printf("\n");	
+	printf("\n");
 	va_end(ap);
 }
