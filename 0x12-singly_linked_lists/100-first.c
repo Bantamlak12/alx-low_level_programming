a2d85c7397425f3babb168dda5b98d79a7170277
Bantamlak <neoluka315@gmail.com>
2022-10-22 22:06:42 +0000

    betty style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * print_string - prints a string before the main
+ * print - prints a string before the main
  * Return: Nothing.
  */
 
-void print_string(void)__attribute__ ((constructor));
-void print_string(void)
+void __attribute__ ((constructor)) print(void)
 {
-	char *str = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+	char *str = "You're beat! and yet, you must allow,\n"
+				"I bore my house upon my back!\n";
 
 	printf("%s", str);
 }
