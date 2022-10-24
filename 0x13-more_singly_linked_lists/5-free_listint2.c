1da02b1c2f49ff4f3cf44ad79f8f02f8e3538194
Bantamlak <neoluka315@gmail.com>
2022-10-24 21:09:14 +0000

    managing memory

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,7 +12,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
 	while (*head)
@@ -21,5 +21,5 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = temp;
 	}
-	*head = NULL;
+	head = NULL;
 }
