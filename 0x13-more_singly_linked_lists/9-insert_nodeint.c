75c7b1029e6011ea7d87c8e539187d86882fff90
Bantamlak <neoluka315@gmail.com>
2022-10-26 14:45:01 +0000

    function added to calculate size

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,27 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+int get_size(listint_t *head);
+
+/**
+ *
+ *
+ *
+ */
+
+int get_size(listint_t *head)
+{
+	listint_t *temp = head;
+	unsigned int size = 0;
+
+	while (temp != NULL)
+	{
+		size++;
+		temp = temp->next;
+	}
+	return (size);
+}
 
 /**
  * insert_nodeint_at_index - inserts a new node at a given position
@@ -13,7 +35,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp = *head, *new;
-/*	unsigned int size = get_size(*head);*/
+	unsigned int size = get_size(*head);
 	unsigned int count = 0;
 
 	if (*head == NULL)
@@ -23,6 +45,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
+	if (idx > size)
+		return (NULL);
 	if (idx == 0)
 	{
 		new->next = temp->next;
