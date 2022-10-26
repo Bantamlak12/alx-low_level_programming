28245be8530fd45585bdbaf30473df084af94dac
Bantamlak <neoluka315@gmail.com>
2022-10-26 15:10:17 +0000

    considering the begining of the node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -44,6 +44,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 	new->next = temp;
+	temp = new;
 	if (idx > size)
 		return (NULL);
 	while (temp != NULL)
