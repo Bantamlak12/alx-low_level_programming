d02dfdcf142653a8916f070fc865f32f7e39105a
Bantamlak <neoluka315@gmail.com>
2022-10-29 18:23:11 +0000

    considering the index if it's greater that 63 bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,14 +7,11 @@
  *
  * Return: the value of the bit at index
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int num = 0;
-
-	if (index > n)
+	if (index > 63)
 		return (-1);
 
-	num = (n >> index) & 1;
-
-	return (num);
+	return ((n >> index) & 1);
 }
