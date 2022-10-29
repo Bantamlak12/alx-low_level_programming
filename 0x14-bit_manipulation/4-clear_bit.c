d10debcd9e511ccd0a7895cb72bbc06c5f15f56d
Bantamlak <neoluka315@gmail.com>
2022-10-29 23:58:42 +0000

    checking and correcting error

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,7 +12,10 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63 || *n < 1)
 		return (-1);
-	*n ^= 1 << index;
+	if (*n <= 1024)
+		*n ^= 1 << index;
+	else
+		*n ^= 0 << index;
 
 	return (1);
 }
