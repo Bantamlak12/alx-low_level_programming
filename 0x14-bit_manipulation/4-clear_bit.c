7f7b96373331f3a088f57145a9af5e014ebe86f6
Bantamlak <neoluka315@gmail.com>
2022-10-29 23:32:48 +0000

    checking n when less than 1

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,10 +10,9 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
+	if (index > 63 || *n < 1)
 		return (-1);
-	if (*n > 1)
-		*n ^= 1 << index;
+	*n ^= 1 << index;
 
 	return (1);
 }
