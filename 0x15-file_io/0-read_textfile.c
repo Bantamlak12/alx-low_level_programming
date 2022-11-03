5a80949fe2a3fcab0807e8bf615cd5073df9dc9e
Bantamlak <neoluka315@gmail.com>
2022-11-03 17:01:02 +0000

    Ajusting lines of code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,26 +16,30 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
+	int fd, r, w;
 	char *buffer;
 
-	if (filename == NULL)
+	if (filename == NULL || letters == '\0')
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	}
+
+	r = read(fd, buffer, letters);
+	if (r == -1)
+		return (0);
+
+	w = write(STDOUT_FILENO, buffer, letters);
+	if (w == -1)
+		return (0);
+
 	free(buffer);
-	close(o);
+	close(fd);
 
 	return (w);
 }
