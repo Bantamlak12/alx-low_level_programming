4097c7317f55bac54f2b9e9eb4ff2156864e78be
Bantamlak <neoluka315@gmail.com>
2022-11-03 22:04:23 +0000

    2-append_text_to_file.c: update signed of write function

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,8 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int fd, len = 0;
+	ssize_t w;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,6 +23,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_WRONLY | O_APPEND, 0600);
 	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
 		return (-1);
 	if (text_content != NULL)
 	{
