2902e6a4a9b37a31bff9cfb6d45e2041fb255da9
Bantamlak <neoluka315@gmail.com>
2022-11-03 21:58:06 +0000

    2-append_text_to_file.c: giving it file permission

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,10 +20,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	len = strlen(text_content);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND, 0600);
 	if (fd == -1)
-		return (-1);
-	if (text_content == NULL)
 		return (-1);
 	if (text_content != NULL)
 	{
