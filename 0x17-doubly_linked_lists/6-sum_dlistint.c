0efd8dabb3231ed3db865a74f86bd06c2fa1a627
Bantamlak <neoluka315@gmail.com>
2022-12-08 20:11:39 +0000

    betty style

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -4,14 +4,14 @@
  * sum_dlistint - sum all the data(n) of a dlistint_t linked list
  * @head: First node of the list
  * Return: sum of all the data of the list
- * 		   0 if the list is empty
+ *		   0 if the list is empty
  */
 
 int sum_dlistint(dlistint_t *head)
 {
 	dlistint_t *ptr;
 	int sum = 0;
-	
+
 	if (head != NULL)
 	{
 		ptr = head;
