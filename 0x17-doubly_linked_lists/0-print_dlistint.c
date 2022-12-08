dbc11cd2442d12d5bcdd656ed3cf7da85ed01332
Bantamlak <neoluka315@gmail.com>
2022-12-08 13:18:01 +0000

    betty style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_dlistint_t - prints all the elements of a dlistint_t list
+ * print_dlistint - prints all the elements of a dlistint_t list
  *
  * @h: a head node
  * Return: the number of nodes
@@ -17,5 +17,5 @@ size_t print_dlistint(const dlistint_t *h)
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return count;
+	return (count);
 }
