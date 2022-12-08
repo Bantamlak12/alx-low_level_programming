e3cac0fef2d1cdf01c287c1d36d3ff3bc4de3dbc
Bantamlak <neoluka315@gmail.com>
2022-12-08 20:09:54 +0000

    first commit of the task

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum all the data(n) of a dlistint_t linked list
+ * @head: First node of the list
+ * Return: sum of all the data of the list
+ * 		   0 if the list is empty
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr;
+	int sum = 0;
+	
+	if (head != NULL)
+	{
+		ptr = head;
+		while (ptr != NULL)
+		{
+			sum += ptr->n;
+			ptr = ptr->next;
+		}
+		return (sum);
+	}
+	else
+		return (0);
+}
