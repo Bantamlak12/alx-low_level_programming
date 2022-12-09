0048e8ff539c368d069079a170c8fff66f7c3fd7
Bantamlak <neoluka315@gmail.com>
2022-12-09 07:14:23 +0000

    updated

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,12 +19,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (idx != 1)
+	for (; idx != 1; idx--)
 	{
 		temp = temp->next;
 		if (temp == NULL)
 			return (NULL);
-		idx--;
 	}
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
