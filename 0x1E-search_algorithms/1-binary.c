754dad4d3e1ca1f2ee468f9405ace3e7b35d2628
Bantamlak <neoluka315@gmail.com>
2023-05-10 08:08:38 +0000

    return null if array is null

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -17,6 +17,9 @@ int binary_search(int *array, size_t size, int value)
 	size_t left = 0;
 	size_t right = size - 1;
 
+	if (array == NULL)
+		return (-1);
+
 	while (left <= right)
 	{
 		mid = (left + right) / 2;
